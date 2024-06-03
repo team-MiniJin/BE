b8d288c4f4f708db9651610f63d7b6a2b75a0c6f
lyckabc <dh2heart@gmail.com>
2024-06-03 19:41:19 +0900

    feat:✨tour API Request Entity added tour 관련 package folder set added TourAPIRequest Entity added

---
+++ src/main/java/com/minizin/travel/tour/domain/entity/TourAPIRequest.java
@@ -0,0 +1,14 @@
+package com.minizin.travel.tour.domain.entity;/**
+ * Class: TourAPIRequest 
+ * Project: travel 
+ * Package: com.minizin.travel.tour.domain.entity 
+ *
+ * Description:
+ * TourAPIRequest 
+ *
+ * @author dong-hoshin 
+ * @date 6/3/24 19:23  
+ * Copyright (c) 2024 MiniJin 
+ * @see <a href="https://github.com/lyckabc">GitHub Repository</a>
+ */public class TourAPIRequest {
+}
