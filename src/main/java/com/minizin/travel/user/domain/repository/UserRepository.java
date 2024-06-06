a69d7230a9b16671d4b2630976072bd3fdd4a64d
sskkilm <sskkilm@naver.com>
2024-06-06 18:12:31 +0900

    feat:✨add user repository

---
+++ src/main/java/com/minizin/travel/user/domain/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.minizin.travel.user.domain.repository;
+
+import com.minizin.travel.user.domain.entity.UserEntity;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsername(String username);
+}
