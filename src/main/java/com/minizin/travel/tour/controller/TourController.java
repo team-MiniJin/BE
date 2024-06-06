0d60f74e4d5d372ce2fd11d93163c1319f1db66b
lyckabc <dh2heart@gmail.com>
2024-06-07 03:00:27 +0900

    feat:✨TourController just java file created

---
+++ src/main/java/com/minizin/travel/tour/controller/TourController.java
@@ -0,0 +1,22 @@
+package com.minizin.travel.tour.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Class: TourController Project: travel Package: com.minizin.travel.tour.controller
+ * <p>
+ * Description: TourController
+ *
+ * @author dong-hoshin
+ * @date 6/7/24 02:58 Copyright (c) 2024 MiniJin
+ * @see <a href="https://github.com/team-MiniJin/BE">GitHub Repository</a>
+ */
+@Api(tags = "wiki Controller")
+@RestController
+@RequestMapping("/wiki-pages")
+@RequiredArgsConstructor
+public class TourController {
+
+}
