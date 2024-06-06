09da35f4ceb75889901c8e65fb32df1bf1e25f89
lyckabc <dh2heart@gmail.com>
2024-06-07 03:32:35 +0900

    feat:✨TourController getAPITourData for Test added

--- src/main/java/com/minizin/travel/tour/controller/TourController.java
+++ src/main/java/com/minizin/travel/tour/controller/TourController.java
@@ -1,6 +1,13 @@
 package com.minizin.travel.tour.controller;
 
+import com.minizin.travel.tour.domain.entity.TourAPIResponse;
+import com.minizin.travel.tour.service.TourService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 6/7/24 02:58 Copyright (c) 2024 MiniJin
  * @see <a href="https://github.com/team-MiniJin/BE">GitHub Repository</a>
  */
-@Api(tags = "wiki Controller")
+@Tag(name = "tour Controller")
 @RestController
-@RequestMapping("/wiki-pages")
+@RequestMapping("/tour")
 @RequiredArgsConstructor
 public class TourController {
-
+    private final TourService tourService;
+    @Operation(summary = "Get Wiki Page by ID", description = "Retrieve a specific WikiPage by its ID")
+    @GetMapping("/areaBasedList")
+    public ResponseEntity<TourAPIResponse> getAPITourData() {
+        return ResponseEntity.ok(tourService.getTourAPIFromSite());
+    }
 }
