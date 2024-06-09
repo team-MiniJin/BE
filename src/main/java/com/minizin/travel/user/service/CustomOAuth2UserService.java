147124b8f0c62f0b746fd0c4e0063836df04ecb3
sskkilm <sskkilm@naver.com>
2024-06-10 06:27:30 +0900

    refactor:♻️ refactor CustomOAuth2UserService - 기존에 상속을 통해 사용하던 defaultOAuth2UserService의 loadUser를 delegate 방식으로 리팩토링

--- src/main/java/com/minizin/travel/user/service/CustomOAuth2UserService.java
+++ src/main/java/com/minizin/travel/user/service/CustomOAuth2UserService.java
@@ -9,6 +9,7 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
@@ -17,15 +18,16 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     private final UserRepository userRepository;
+    private final DefaultOAuth2UserService delegate;
 
     @Override
     @Transactional
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
-        OAuth2User oAuth2User = super.loadUser(userRequest);
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
         KakaoResponse kakaoResponse = null;
