154fed4b89d189ffbb5985161b0472be4fa478e4
sskkilm <sskkilm@naver.com>
2024-06-12 04:35:38 +0900

    feat:✨add in permitAll path

--- src/main/java/com/minizin/travel/config/SecurityConfig.java
+++ src/main/java/com/minizin/travel/config/SecurityConfig.java
@@ -87,7 +87,7 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/auth/join", "/auth/login").permitAll()
+                        .requestMatchers("/", "/auth/join", "/auth/login", "/auth/jwt").permitAll()
                         .anyRequest().authenticated());
 
         // UsernamePasswordAuthenticationFilter 자리에 LoginFilter 추가
