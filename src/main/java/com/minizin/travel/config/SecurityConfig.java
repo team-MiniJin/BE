4e30c3c66dc089128f7752d312071309ab95137e
sskkilm <sskkilm@naver.com>
2024-06-17 23:29:00 +0900

    refactor:♻️ add ignore path

--- src/main/java/com/minizin/travel/config/SecurityConfig.java
+++ src/main/java/com/minizin/travel/config/SecurityConfig.java
@@ -106,7 +106,8 @@ public class SecurityConfig  {
                 log.debug("Configuring URL authorization");
                 auth.requestMatchers("/", "/auth/join", "/auth/login", "/auth/jwt",
                             "/mails/auth-code", "/mails/auth-code/verification", "/tour/**",
-                                "/users/find-id", "/users/find-password", "/plans/**").permitAll()
+                                "/users/find-id", "/users/find-password", "/plans/**",
+                                "/scraps/**", "/chat/**", "/s3/**", "/travels/**").permitAll()
                     .anyRequest().authenticated();
             });
 
