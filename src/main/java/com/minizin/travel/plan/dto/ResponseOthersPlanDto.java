5715314f1dd6b58e1d405ccf6d7804b8bed28e16
aNN-algorithm <kyukyu0314@naver.com>
2024-06-20 03:59:27 +0900

    fix:🐛 #151 조회할 데이터가 없을 시 next_cursor 필드 제거

--- src/main/java/com/minizin/travel/plan/dto/ResponseOthersPlanDto.java
+++ src/main/java/com/minizin/travel/plan/dto/ResponseOthersPlanDto.java
@@ -1,5 +1,6 @@
 package com.minizin.travel.plan.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
@@ -14,6 +15,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class ResponseOthersPlanDto {
 
