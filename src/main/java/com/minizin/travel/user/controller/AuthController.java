43650e9118da1e95de9c0d428b8cc6627ccb9d96
sskkilm <sskkilm@naver.com>
2024-06-21 00:32:22 +0900

    refactor:♻️ refactor authorization path

--- src/main/java/com/minizin/travel/user/controller/AuthController.java
+++ src/main/java/com/minizin/travel/user/controller/AuthController.java
@@ -7,14 +7,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
@@ -23,21 +21,16 @@ public class AuthController {
 
     @GetMapping("/auth/jwt")
     public ResponseEntity<?> getJwt(HttpServletRequest request, HttpServletResponse response) {
-        log.error("/auth/jwt start");
         Cookie[] cookies = request.getCookies();
         if (cookies != null) {
-            log.error("there is cookie");
             for (Cookie cookie : cookies) {
-                log.error("there is cookie : " + cookie.getName());
                 if (cookie.getName().equals("Authorization")) {
-                    log.error("there is Authorization cookie");
                     String token = cookie.getValue();
                     response.setHeader("Authorization", "Bearer " + token);
                     return ResponseEntity.ok("JWT 헤더로 발급");
                 }
             }
         }
-        log.error("cookie is null");
         return ResponseEntity.status(401).body("Unauthorized");
     }
 
