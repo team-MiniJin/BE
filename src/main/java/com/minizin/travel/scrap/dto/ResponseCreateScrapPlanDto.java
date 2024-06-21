66e384192c088a51d923dcd5adfc4dd470d9fae3
aNN-algorithm <kyukyu0314@naver.com>
2024-06-21 17:07:30 +0900

    fix:🐛 북마크 관련 예외처리를 응답 메시지로 응답

--- src/main/java/com/minizin/travel/scrap/dto/ResponseCreateScrapPlanDto.java
+++ src/main/java/com/minizin/travel/scrap/dto/ResponseCreateScrapPlanDto.java
@@ -1,5 +1,6 @@
 package com.minizin.travel.scrap.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.minizin.travel.scrap.entity.Scrap;
 import lombok.Builder;
@@ -9,6 +10,7 @@ import java.time.format.DateTimeFormatter;
 
 @Builder
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseCreateScrapPlanDto {
 
     @JsonProperty("scrap_id")
@@ -20,12 +22,26 @@ public class ResponseCreateScrapPlanDto {
 
     private String createdAt;
 
+    private boolean success;
+
+    private String message;
+
     public static ResponseCreateScrapPlanDto toDto(Scrap scrap) {
         return ResponseCreateScrapPlanDto.builder()
+                .success(true)
                 .id(scrap.getId())
                 .userId(scrap.getUserId())
                 .planId(scrap.getPlanId())
                 .createdAt(scrap.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+                .build();
+    }
+
+    public static ResponseCreateScrapPlanDto fail(Long planId, String message) {
+
+        return ResponseCreateScrapPlanDto.builder()
+                .success(false)
+                .message(message)
+                .id(planId)
                 .build();
     }
 }
